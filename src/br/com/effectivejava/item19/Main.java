92f83e68a2d6e5407819d57f6f4b3b78fbf56911
HyperClass01 <HyperClass01@HyperClass01-PC>
2016-11-07 19:03:30 -0200

    Add item 19: So use interface para definir tipos.

---
+++ src/br/com/effectivejava/item19/Main.java
@@ -0,0 +1,33 @@
+package br.com.effectivejava.item19;
+
+/**
+ * Item 19: So use interfaces para definir tipos
+ * 
+ * 
+ * Quando uma classe implementa uma interface, essa serve como um tipo que pode
+ * ser usado para referenciar instancias da classe, o fato de uma classe
+ * implementar uma interface deve dizer algo sobre que um cliente pode fazer com
+ * instancias da classe.
+ * 
+ * Um tipo de interface que nao passa nesse teste e a chamada interface de
+ * constantes. Esta interface nao contem metodos, ela e composta apenas por
+ * campos finais estaticos, cada um exportando uma constante.
+ * 
+ * O padrao de interface de constantes e uma forma inadequada do uso de
+ * interface.
+ * 
+ * A implementacao de uma interface de constantes faz com que esse detalhe
+ * vaze para API exportada da classe.
+ * 
+ * O pior e representar um compremetimento: se em uma versao futura a classe
+ * for modificado para que nao precise mais usar constantes, continuara tendo
+ * que implementar a interface para assegurar a compatibilidade binaria.
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/br/com/effectivejava/item19/PhysicalConstants.java
@@ -0,0 +1,14 @@
+package br.com.effectivejava.item19;
+
+public interface PhysicalConstants {
+	
+	// Numero de Avogadro (1/mol
+	static final double AVOGRADOS_NUMBER = 6.02214199e23;
+	
+	// Constante de Boltzman (Constante que relaciona temperatura e energia de moleculas)
+	static final double BOLTZMANN_CONSTANT = 1.3896503e-23;
+	
+	// Massa do eletron (kg)
+	static final double ELECTRON_MASS = 9.10938188e-31;
+	
+}
