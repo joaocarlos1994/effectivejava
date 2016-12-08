aa670b0ef0cdc3e7ab2eaecb784d5fcfc0f85388
HyperClass01 <HyperClass01@192.168.15.27>
2016-12-08 18:56:54 -0200

    Iniciando estudos do item 28.

---
+++ src/br/com/effectivejava/item28/Main.java
@@ -0,0 +1,17 @@
+package br.com.effectivejava.item28;
+
+import java.util.List;
+
+/**
+ * Item 28: Use curingas restritos para aumentar a flexibilidade da API
+ * */
+public class Main {
+	
+	public <E> List<E> metodoGeneric() {
+		return null;
+	}
+	
+	public static void main(final String[] args) {
+			
+	}
+}
